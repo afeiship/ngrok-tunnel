608d276dc6a45004c2b961be327dc219131253c7
afei <88603982@qq.com>
2016-05-03 14:04:41 +0800

    initial

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ionicWechatApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'toastr']);
+
+})();

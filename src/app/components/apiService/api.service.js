92fdbdad75720ef0ebe353add38295fcd8c1c4fa
afei <88603982@qq.com>
2016-05-05 09:55:28 +0800

    optimize api.service

--- src/app/components/apiService/api.service.js
+++ src/app/components/apiService/api.service.js
@@ -8,26 +8,32 @@
   var APISERVER = '';
 
   /** @ngInject */
-  function ApiService() {
+  function ApiService($http) {
     //base methods:
     this.REQUEST = function (inMethod, inName, inOptions) {
       return $http(
         angular.extend({
           method: inMethod,
           dataType: 'json',
-          url: APISERVER + inName,
-          headers: headers
+          url: _getUrl(inName)
         }, inOptions)
       );
     };
 
     this.GET = function (inName, inOptions) {
-      return this.REQUEST('GET', inName, inOptions);
+      var url = _getUrl(inName);
+      return $http.get(url, inOptions);
     };
 
     this.POST = function (inName, inOptions) {
-      return this.REQUEST('POST', inName, inOptions);
+      var url = _getUrl(inName);
+      return $http.post(url, inOptions.data || {}, inOptions);
     };
+
+
+    function _getUrl(inName) {
+      return APISERVER + inName;
+    }
   }
 
 })();

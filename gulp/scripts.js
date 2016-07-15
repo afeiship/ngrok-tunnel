a60ef33482ebe07f27084f5aa1e73954f62d011a
feizheng <1290657123@qq.com>
2016-07-15 08:26:33 +0800

    wip

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -20,7 +20,7 @@ gulp.task('scripts', function() {
 
 function buildScripts() {
   return gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
-    .pipe($.eslint())
-    .pipe($.eslint.format())
+    // .pipe($.eslint())
+    // .pipe($.eslint.format())
     .pipe($.size())
 };
